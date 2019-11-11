354bd9f44fca7d4da50074b2e1f7c764bced4069
DAITADOKORO <bangasa0101@gmail.com>
2019-11-11 14:02:58 +0900

    Finish 提出課題２

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+  def index
+  	@book = Book.new
+  	@books = Book.all
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+  def create
+  	@book = Book.new(book_params)
+    if @book.save
+  	 redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+      # flash[:error] = "can't be blank"
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path, notice: 'Book was successfully destroyed.'
+  	end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+end
